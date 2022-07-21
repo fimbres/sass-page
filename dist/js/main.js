c6bdbe43553b171f0ac55466436e086f66e52c1d
Isaac <ifimbres@uabc.edu.mx>
2022-07-20 21:34:55 -0700

    finish details to the home page

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const hamburguer = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
 
@@ -12,12 +13,14 @@ function toggleMenu() {
         hamburguer.classList.add('open');
         nav.classList.add('open');
         menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
         showMenu = true;
     }
     else {
         hamburguer.classList.remove('open');
         nav.classList.remove('open');
         menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
         showMenu = false;
     }
 }
